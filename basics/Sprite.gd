2d79d8435f77093a97bdad1117c0172e804269ea
Santyru <santiago0988@hotmail.com>
2023-01-26 19:47:31 -0300

    Script movement

--- basics/Sprite.gd
+++ basics/Sprite.gd
@@ -8,7 +8,9 @@ var angular_speed = 2 * PI
 
 func _process(delta):
 	rotation += angular_speed * delta
-
 	var velocity = Vector2.UP.rotated(rotation) * speed
-
 	position += velocity * delta
+
+
+func _on_Button_pressed():
+	set_process(not is_processing())
