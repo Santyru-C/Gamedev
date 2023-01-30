9a5720a7bdeb72e5e9948a9110625f74148c7a39
Santyru <santiago0988@hotmail.com>
2023-01-30 19:57:48 -0300

    Add script to ball

---
+++ Pong/gamedata/ball.gd
@@ -0,0 +1,16 @@
+extends RigidBody2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var position = Vector2
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print(Vector2)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
