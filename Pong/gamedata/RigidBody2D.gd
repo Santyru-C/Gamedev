7fcd04e337e108e3b292ab56398f019f65533e4e
Santyru <santiago0988@hotmail.com>
2023-01-30 23:48:11 -0300

    Add scripting to ball

--- Pong/gamedata/RigidBody2D.gd
+++ Pong/gamedata/RigidBody2D.gd
@@ -1,24 +1,18 @@
 extends RigidBody2D
 
+var speed = 20
+
+func _process(delta):
+	position += Vector2.UP * speed * delta
 
 # Declare member variables here. Examples:
 # var a = 2
-# var b = "text
-var speed = 20
+# var b = "text"
 
-func _init():
-	print(speed)
-	print(Vector2())
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
-	
-func _process(delta):
-	position += Vector2.DOWN * speed * delta
-	
-	if is_colliding():
-		print("colliding")
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
