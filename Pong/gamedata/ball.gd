e6fe31ac6ed4ca3997a4d50bad35dafa312abd63
Santyru <santiago0988@hotmail.com>
2023-02-10 22:11:47 -0300

    add movement and bounce

--- Pong/gamedata/ball.gd
+++ Pong/gamedata/ball.gd
@@ -1,20 +1,23 @@
 extends KinematicBody2D
 
-export (int) var speed = 50
+export (int) var speed = 100
 
 var rng = RandomNumberGenerator.new()
-var initial_rotation = 1
+var initial_rotation = 0
 var direction = Vector2()
 var velocity = Vector2()
 
 func _ready():
 	rng.randomize()
-	initial_rotation = 50#rng.randf_range(-3.14, 3.14)
+	initial_rotation = rng.randf_range(-3.14, 3.14)
 	direction = Vector2(1, 1).rotated(initial_rotation)
 	
 func _physics_process(delta):
 	velocity = direction * speed
-
 	var collides = move_and_collide(velocity * delta)
+	
+	if collides:
+		direction = direction.bounce(collides.normal)
+		speed += 25
 
 
