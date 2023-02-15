1a61a96623ffaf83edadf531c0acaf18ab03a95b
Santyru <santiago0988@hotmail.com>
2023-02-14 21:30:44 -0300

    Add random initial rotation

--- Pong/gamedata/ball.gd
+++ Pong/gamedata/ball.gd
@@ -3,14 +3,16 @@ extends KinematicBody2D
 export (int) var speed = 100
 
 var rng = RandomNumberGenerator.new()
-var initial_rotation = 0
-var direction = Vector2()
+var rotation_list = [PI/4, -PI/4, 3*PI/4, -3*PI/4]
+var direction = Vector2(1, 0)
 var velocity = Vector2()
 
 func _ready():
 	rng.randomize()
-	initial_rotation = rng.randf_range(-3.14, 3.14)
-	direction = Vector2(1, 1).rotated(initial_rotation)
+	var initial_rotation = rotation_list[rng.randi_range(0,3)]
+	var offset = rng.randf_range(-PI/8, PI/8)
+	direction = direction.rotated(initial_rotation + offset)
+	print(direction)
 	
 func _physics_process(delta):
 	velocity = direction * speed
