ef6c57b2c34586282f50de81b93e8db9dfe51684
Santyru <santiago0988@hotmail.com>
2023-02-20 21:05:28 -0300

    add updated start game function

--- Pong/gamedata/Main.gd
+++ Pong/gamedata/Main.gd
@@ -6,16 +6,14 @@ var ball = ball_scene.instance()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$start_timer.start()
-	
-func _on_Timer_timeout():
-	add_child(ball)
+	pass
 
-
-func _on_goal_timer_timeout():
+func drop_ball():
 	ball = ball_scene.instance()
 	add_child(ball)
-
+	
+func _on_goal_timer_timeout():
+	drop_ball()
 
 func _on_goal_1_body_exited(body):
 	body.queue_free()
@@ -28,3 +26,10 @@ func _on_goal_2_body_exited(body):
 	body.queue_free()
 	print("point")
 	$goal_timer.start()
+
+
+func _on_HUD_start_game():
+	$HUD/message_timer.start()
+	yield(get_tree().create_timer(4.5), "timeout")
+	drop_ball()
+	
