b8d5d2204cb55f89868b7914cbd92cb86f0cb22c
Santyru <santiago0988@hotmail.com>
2023-02-21 21:20:26 -0300

    add simple AI script

---
+++ Pong/gamedata/AI.gd
@@ -0,0 +1,30 @@
+extends KinematicBody2D
+export (int) var speed = 400
+
+var ball_position_y = Vector2()
+var racket_position_y = Vector2()
+var direction = Vector2()
+var velocity = Vector2()
+
+func _ready():
+	pass
+
+func follow(ball_position):
+	racket_position_y = self.get_position().y
+	if (ball_position - racket_position_y) < -5:
+		direction.y = -1
+	elif (ball_position - racket_position_y) > 5:
+		direction.y = 1
+	else:
+		direction.y = 0
+	
+func _physics_process(_delta):
+	var ball = get_tree().get_root().get_node("Main").ball
+	
+	if is_instance_valid(ball):
+		follow(ball.get_position().y)
+	else:
+		direction.y = 0
+		
+	velocity = move_and_slide(direction * speed)
+	
