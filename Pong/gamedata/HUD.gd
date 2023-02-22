f4fbb865d2977c7329765ab065bcc2cbb0aac0ca
Santyru <santiago0988@hotmail.com>
2023-02-21 21:23:16 -0300

    change HUD display

--- Pong/gamedata/HUD.gd
+++ Pong/gamedata/HUD.gd
@@ -1,36 +1,29 @@
 extends CanvasLayer
-
 signal start_game
+
 var count = 0
 
 func display_start_text(n):
-	var text_lib = ["1", "2", "3", "Pong"]
+	var text_lib = ["3", "2", "1", "Pong!"]
 	$message.text = text_lib[n]
 	
 func _ready():
 	pass
-#apretas el boton
-#el boton desparece
-#inicia el temporizador
-#primer tick display 1
-#segundo tick display 2
-#tercer tick display 3
-# cuarto tick display pong
-# detener timer
-#comienza el juego
 
 func _on_message_timer_timeout():
+	print("timer started")
+	print(count)
+	# try using label.show()
 	if count == 4:
 		$message_timer.stop()
-		$message.text = " "
+		$message.text = ""
 		$message.hide()
+		count = 0
 	
 	else:	
 		display_start_text(count)
 		count += 1
-	
-
-
+		
 func _on_start_pressed():
 	emit_signal("start_game")
 	$start.hide()
